ac4825e1c4e97c8c614310424e47b14e1a9d6a2a
willijo4 <willijo4@oregonstate.edu>
2017-09-15 22:29:27 -0400

    Making my own branch

---
+++ server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var path = require('path');
+var parser = require('body-parser');
+var mongo = require('mongodb').MongoClient;
+
+var app = express();
+var port = 8000;
+var db = 'healthchat';
+var url = 'mongodb://localhost:27017/' + db;
+
+app.set('port', port);
+app.set('view engine', 'ejs');
+
+app.use(parser.json());
+app.use(parser.urlencoded({ extended: true }));
+
+app.get('/', function(req, res) {
+	res.render('login');
+});
+
+app.post('/home', function(req, res) {
+	mongo.connect(url, function(err, db) {
+		db.collection('users').findOne({
+			username: req.body.username
+		}, function(err, result) {
+			if (result === null || result.password !== req.body.password) {
+				res.render('login');
+			} else {
+				res.render('home', { username: req.body.username });
+			}	
+			db.close();
+		});
+	});
+});
+
+app.listen(port);
